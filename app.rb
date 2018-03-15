cd699d1e3e8d63d485ea3c19e93201d436fd1be8
Chris Henry <cdhenry@gmail.com>
2018-03-15 19:54:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinized' do
-    @piglatinized_text = PigLatinizer.new.piglatinize(:user_phrase)
+    @piglatinized_text = PigLatinizer.new.piglatinize(:user_input)
 
     erb :piglatinized
   end
