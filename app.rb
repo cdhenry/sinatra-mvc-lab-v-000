22fe13c1db587eaf1b09c1dfa585f656ee5c8f41
Chris Henry <cdhenry@gmail.com>
2018-03-15 19:10:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/user_input' do
-    
+    erb :user_input
   end
 
   post '/piglatinized' do
     
+    erb :piglatinized
   end
 end
