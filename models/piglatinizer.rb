637da00c7a9e70bcb4f462d676092f01dcbb11a5
Chris Henry <cdhenry@gmail.com>
2018-03-15 19:17:13 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,13 @@
 class PigLatinizer
+  attr_reader :text
+
+  def initialize(text)
+    @text = text
+  end
   #For words that begin with consonant sounds,
   #all letters before the initial vowel are placed at the end of the word sequence.
   #Then, "ay" is added
-
+  
   #When words begin with consonant clusters (multiple consonants that form one sound),
   #the whole sound is added to the end when speaking or writing.
 
